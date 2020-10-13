a438ca4ac0e804fb54a22c799b2d84a01ede14c4
Gaye0523 <maya_1819@hotmail.com>
2020-10-13 11:35:21 -0500

    Iniciamos el proyecto, obtener el Dom del html

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
 module.exports = () => {
-  // ...
+
 };
+
+const archivo = './README.md';
+const path = require('path');
+const fs = require('fs');
+const md = require('markdown-it')();
+const { JSDOM } = require('jsdom');
+const { rejects } = require('assert');
+const { resolve } = require('path');
+
+
+const rutaAbsoluta = path.resolve(archivo); //Estamos volviendo absoluta
+//console.log(rutaAbsoluta)
+
+function readFile() {
+  const promesa = new Promise((resolve, reject) => {
+    fs.readFile(rutaAbsoluta, { encoding: 'utf8' }, function (error, data) {
+      if (error) {
+        const err = new Error(`Error: ${error}`)
+        reject(err);
+      } else {
+        resolve(data)
+        let txtPlano = data;
+      }
+    })
+  })
+  return promesa
+}
+
+
+const mdLink = () => {
+  
+  readFile()
+  .then((resultado) => {
+    const readHtml = new Promise ((resolve) => {
+      let result = md.render(resultado);
+      resolve(result)
+    })
+    return(readHtml)
+  })
+
+  .then((seeHtml) => {
+    let dom = new JSDOM(seeHtml)
+    const documet = dom.window.document;
+    console.log(documet)
+  })
+}
+mdLink()
+
+/* .then ((resultado) => 
+  let result = md.render(resultado);
+  //console.log(result)
+}); */
+
