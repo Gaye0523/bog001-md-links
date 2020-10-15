489e523aee0818b8578a210bac6a789533fdb1c3
Gaye0523 <maya_1819@hotmail.com>
2020-10-15 09:45:50 -0500

    Terminacion de funcion MDLINK

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = () => {
 
 };
 
-const archivo = './README.md';
+const parametroCli = './README.md';
 const path = require('path');
 const fs = require('fs');
 const md = require('markdown-it')();
@@ -11,11 +11,12 @@ const { rejects } = require('assert');
 const { resolve } = require('path');
 
 
-const rutaAbsoluta = path.resolve(archivo); //Estamos volviendo absoluta
+//const rutaAbsoluta = path.resolve(archivo); //Estamos volviendo absoluta
 //console.log(rutaAbsoluta)
 
-function readFile() {
+function readFile(archivo) {
   const promesa = new Promise((resolve, reject) => {
+    const rutaAbsoluta = path.resolve(archivo)
     fs.readFile(rutaAbsoluta, { encoding: 'utf8' }, function (error, data) {
       if (error) {
         const err = new Error(`Error: ${error}`)
@@ -29,28 +30,50 @@ function readFile() {
   return promesa
 }
 
-
-const mdLink = () => {
-  
-  readFile()
-  .then((resultado) => {
-    const readHtml = new Promise ((resolve) => {
-      let result = md.render(resultado);
-      resolve(result)
-    })
-    return(readHtml)
-  })
-
-  .then((seeHtml) => {
-    let dom = new JSDOM(seeHtml)
-    const documet = dom.window.document;
-    console.log(documet)
-  })
+const extracLink = (texto, archivo) => {
+  let seeHtml = md.render(texto);
+  let dom = new JSDOM(seeHtml)
+  const seeLink = dom.window.document;
+  const showLinks = seeLink.querySelectorAll('a');
+  console.log(showLinks)
+  const rutaAbsoluta = path.resolve(archivo);
+  const arrayLinks = []
+  showLinks.forEach(link => {
+    const objetLink = {
+      href: link.href,
+      text: link.text,
+      file: rutaAbsoluta
+    }
+    arrayLinks.push(objetLink)
+  });
+  return arrayLinks
 }
-mdLink()
 
-/* .then ((resultado) => 
-  let result = md.render(resultado);
-  //console.log(result)
-}); */
+
+const mdLink = (archivo) => {
+  return readFile(archivo)
+    .then((resultado) => {
+      const readHtml = new Promise((resolve) => {
+        const resultLink = extracLink(resultado, archivo)
+        resolve(resultLink)
+      })
+      return (readHtml)
+    })
+}
+mdLink(parametroCli)
+
+  .then(links => {
+    console.log(links)
+  })
+  .catch(console.error);
+
+
+
+/* const getLink = () => {
+  const showLinks = showDom.querySelectorAll('a');
+  console.log(showLinks)
+
+}
+getLink() */
+
 
