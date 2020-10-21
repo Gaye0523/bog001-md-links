b13da4b3291b84335b297047bdb4f7ba76097c44
Gaye0523 <maya_1819@hotmail.com>
2020-10-21 16:19:43 -0500

    Incluir la validacion

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ module.exports = () => {
 
 };
 
-const parametroCli = './README.md';
+const parametroCli = './prueba.md';
 const path = require('path');
 const fs = require('fs');
 const md = require('markdown-it')();
+const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const { rejects } = require('assert');
 const { resolve } = require('path');
 
-
-//const rutaAbsoluta = path.resolve(archivo); //Estamos volviendo absoluta
-//console.log(rutaAbsoluta)
 
 function readFile(archivo) {
   const promesa = new Promise((resolve, reject) => {
@@ -35,22 +33,24 @@ const extracLink = (texto, archivo) => {
   let dom = new JSDOM(seeHtml)
   const seeLink = dom.window.document;
   const showLinks = seeLink.querySelectorAll('a');
-  console.log(showLinks)
   const rutaAbsoluta = path.resolve(archivo);
   const arrayLinks = []
   showLinks.forEach(link => {
-    const objetLink = {
-      href: link.href,
-      text: link.text,
-      file: rutaAbsoluta
+    //verificacion que sea una url
+    let validLink = (/^(ftp|http|https):\/\/[^ "]+$/.test(link.href));
+    if (validLink === true) {
+      const objetLink = {
+        href: link.href,
+        text: link.text,
+        file: rutaAbsoluta
+      }
+      arrayLinks.push(objetLink)
     }
-    arrayLinks.push(objetLink)
   });
   return arrayLinks
 }
 
-
-const mdLink = (archivo) => {
+const mdLinks = (archivo) => {
   return readFile(archivo)
     .then((resultado) => {
       const readHtml = new Promise((resolve) => {
@@ -60,20 +60,61 @@ const mdLink = (archivo) => {
       return (readHtml)
     })
 }
-mdLink(parametroCli)
+mdLinks(parametroCli)
 
-  .then(links => {
-    console.log(links)
+
+  .then(res => {
+    console.log(res)
   })
   .catch(console.error);
 
 
-
-/* const getLink = () => {
-  const showLinks = showDom.querySelectorAll('a');
-  console.log(showLinks)
-
+/* const validate = (archivo) => {
+  mdLinks(archivo)
+    .then((objetLink) => {
+      const newObjetLinks = objetLink.map((runObjet) => {
+        let link = runObjet.href;
+        //Crear un objeto nuevo con las mismas propiedad de runObjet, añadir dos propiedades que (status, statustxt )
+        const rutaAbsoluta = path.resolve(archivo);
+        const newObjet = {
+          href: runObjet.href,
+          text: runObjet.text,
+          file: rutaAbsoluta
+        }
+        const responseAxios = new Promise((resolve, reject) => {
+          axios.get(link)
+            .then(res => {
+              const statusLink = res.status;
+              if (statusLink == 200) {
+                newObjet.status = res.status;
+                newObject.statusText = 'Ok';
+                resolve(newObjet)
+              } else {
+                newObjet.status = response.status;
+                newObjet.statusText = 'Fail';
+              }
+            })
+            .catch((err) => {
+              newObjet.status = 404;
+              newObjet.statusText = 'Fail';
+              resolve(newObjet)
+            })
+        })
+        return (responseAxios)
+      })
+    })
 }
-getLink() */
+validate(parametroCli) */
 
+/*   .then(res => {
+    console.log(res)
+  })
+  .catch(console.error); */
+
+//que deber retornar en el callback
+//verificar con un expesion regular para que comience con la expesion https
+//newObject.status =
+//newObject.status = ‘ok’
+//newObject.status = ‘fail’
+//Devuelva una promesa que resuelve un array de objetos con las nuevas propiedad
 
---
+++ prueba.md
@@ -0,0 +1,3 @@
+[Si valida](https://github.com/cheeriojs/cheerio)
+[Si valida](https://www.facelooooo.com/)
+[No valida] (#1-preámbulo)
