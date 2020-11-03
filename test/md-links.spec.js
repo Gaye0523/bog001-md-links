3689f384a401ace5a0804180c6748070fc927ce8
Gaye0523 <maya_1819@hotmail.com>
2020-11-03 12:23:01 -0500

    Entregable final

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -18,7 +18,7 @@ describe('Mostrar las Estadisticas de un array de Objetos', () => {
     }
   ]
 
-  test('Igualdad de Objetos que recibe', () => {
+  xtest('Igualdad de Objetos que recibe', () => {
     const respuesta = {
       total: 2,
       unique: 2
@@ -26,7 +26,7 @@ describe('Mostrar las Estadisticas de un array de Objetos', () => {
     expect(stats(newArrayObjets)).toStrictEqual(respuesta)
   })
 
-  test('Incluir los links rotos', () => {
+  xtest('Incluir los links rotos', () => {
     const broken = true;
     const respuesta = {
       total: 2,
@@ -35,6 +35,52 @@ describe('Mostrar las Estadisticas de un array de Objetos', () => {
     };
     expect(stats(newArrayObjets, broken)).toStrictEqual(respuesta)
   })
+});
+
+describe('Probando Promesa', () => {
+  xtest('Retorno de un array de objetos', done => {
+    const ruta = './prueba.md'
+    const response = [
+      {
+        href: 'https://github.com/cheeriojs/cheerio',
+        text: 'Si valida',
+        file: 'C:\\Users\\Gaye Bernal\\Desktop\\Maye\\Laboratoria\\Proyecto 4\\bog001-md-links\\prueba.md'
+      },
+      {
+        href: 'https://www.facelooooo.com/',
+        text: 'Si valida',
+        file: 'C:\\Users\\Gaye Bernal\\Desktop\\Maye\\Laboratoria\\Proyecto 4\\bog001-md-links\\prueba.md'
+      }
+    ]
+    return mdLinks(ruta).then(data => {
+      expect(data).toStrictEqual(response);
+      done();
+    });
+  });
+
+  test('Resuelve un array de objetos', done => {
+    const respuesta = [
+      {
+        href: 'https://github.com/cheeriojs/cheerio',
+        text: 'Si valida',
+        file: 'C:\\Users\\Gaye Bernal\\Desktop\\Maye\\Laboratoria\\Proyecto 4\\bog001-md-links\\prueba.md',
+        status: 200,
+        statusText: 'Ok'
+      },
+      {
+        href: 'https://www.facelooooo.com/',
+        text: 'Si valida',
+        file: 'C:\\Users\\Gaye Bernal\\Desktop\\Maye\\Laboratoria\\Proyecto 4\\bog001-md-links\\prueba.md',
+        status: 404,
+        statusText: 'Fail'
+      }
+    ]
+    return mdLinks(ruta, options).then(data => {
+      console.log(data)
+      expect(data).toStrictEqual(respuesta)
+      done();
+    });
+  });
 });
 
 
